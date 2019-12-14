ffe7a91bbf488685665b0e4168338df0b2f5bf99
Rebecca D <rydagostino@gmail.com>
2019-12-14 11:52:22 -0500

    fixed ata

--- checkdata.py
+++ checkdata.py
@@ -15,5 +15,6 @@ data = pd.read_csv('NYC_Jobs.csv')
 #print(data.columns)
 dataWaned = data.loc[:,['JobCategory','#OfPositions']]
 print(data.groupby(['JobCategory']).sum())
+dataWaned = data.groupby(['JobCategory']).sum()
 
 dataWaned.to_csv('job_category_count.csv')
