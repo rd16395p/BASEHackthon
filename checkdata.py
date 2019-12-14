e0223c8edf2363863a47ed646a7364d8d2e82752
Rebecca D <rydagostino@gmail.com>
2019-12-14 11:46:58 -0500

    new data

--- checkdata.py
+++ checkdata.py
@@ -3,9 +3,17 @@ import pandas as pd
 
 data = pd.read_csv('NYC_Jobs.csv')
 
-print(data.head())
+#print(data.head())
 
-print(data['Job Category'].value_counts())
+#print(data['Job Category'].value_counts())
+
 
 # count per job category, number of positions that are open (# Of Positions), export to csv
 # how long are they open, >30 days
+
+#dataWaned = data['JobCategory','#OfPositions']
+#print(data.columns)
+dataWaned = data.loc[:,['JobCategory','#OfPositions']]
+print(data.groupby(['JobCategory']).sum())
+
+dataWaned.to_csv('job_category_count.csv')
