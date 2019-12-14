a961ce8d7aeba9199edc862d8e242dbd79c02e12
Rebecca D <rydagostino@gmail.com>
2019-12-14 12:35:47 -0500

    linear regression for prediction

--- predictionmodel.py
+++ predictionmodel.py
@@ -1,1 +1,96 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+data = pd.read_csv('job_category_count.csv')
+print(data.head())
+
+data['code'] = pd.Categorical.from_array(data.JobCategory).codes
+print(data['code'].head())
+
+''' Does not work, has low results
+X = data['code'].values.reshape(-1,1)
+y = data['#OfPositions'].values.reshape(-1,1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train) #training the algorithm
+
+#To retrieve the intercept:
+print(regressor.intercept_)#For retrieving the slope:
+print(regressor.coef_)
+
+y_pred = regressor.predict(X_train)
+
+df = pd.DataFrame({'Actual': y_train.flatten(), 'Predicted': y_pred.flatten()})
+
+print(df)
+'''
+
+x = data['code'].values.reshape(-1,1)
+y = data['#OfPositions'].values.reshape(-1,1)
+
+n = len(x) # Number of data points
+
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+
+
+W = tf.Variable(np.random.randn(), name = "W")
+b = tf.Variable(np.random.randn(), name = "b")
+
+learning_rate = 0.01
+training_epochs = 1000
+
+# Hypothesis
+y_pred = tf.add(tf.multiply(X, W), b)
+
+# Mean Squared Error Cost Function
+cost = tf.reduce_sum(tf.pow(y_pred-Y, 2)) / (2 * n)
+
+# Gradient Descent Optimizer
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Global Variables Initializer
+init = tf.global_variables_initializer()
+
+
+# Starting the Tensorflow Session
+with tf.Session() as sess:
+
+    # Initializing the Variables
+    sess.run(init)
+
+    # Iterating through all the epochs
+    for epoch in range(training_epochs):
+
+        # Feeding each data point into the optimizer using Feed Dictionary
+        for (_x, _y) in zip(x, y):
+            sess.run(optimizer, feed_dict = {X : _x, Y : _y})
+
+        # Displaying the result after every 50 epochs
+        if (epoch + 1) % 50 == 0:
+            # Calculating the cost a every epoch
+            c = sess.run(cost, feed_dict = {X : x, Y : y})
+            print("Epoch", (epoch + 1), ": cost =", c, "W =", sess.run(W), "b =", sess.run(b))
+
+    # Storing necessary values to be used outside the Session
+    training_cost = sess.run(cost, feed_dict ={X: x, Y: y})
+    weight = sess.run(W)
+    bias = sess.run(b)
+
+
+# Calculating the predictions
+predictions = weight * x + bias
+print("Training cost =", training_cost, "Weight =", weight, "bias =", bias, '\n')
+
+
+# Plotting the Results
+fig = plt.plot(x, y, 'ro', label ='Original data')
+plt.plot(x, predictions, label ='Fitted line')
+plt.title('Linear Regression Result')
+plt.legend()
+plt.show()
+#fig.savefig('temp.png', dpi=fig.dpi)
